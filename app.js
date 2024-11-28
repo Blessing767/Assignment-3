5ce9eb0b98b42879e6c5c5129d5a9ebbca9f2843
Blessing767 <blessing.muhwezi@ontariotechu.net>
2024-11-28 16:16:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // MongoDB connection
-mongoose.connect(dbc.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(conn.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected successfully'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
